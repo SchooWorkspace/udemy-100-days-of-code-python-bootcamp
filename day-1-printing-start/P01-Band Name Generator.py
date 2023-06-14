d60284dfd258d458269e958b08d48ff017ccc52e
SchooWorkspace <schoo.workspace@gmail.com>
2023-06-13 21:15:56 -0700

    commit

--- day-1-printing-start/P01-Band Name Generator.py
+++ day-1-printing-start/P01-Band Name Generator.py
@@ -5,7 +5,7 @@ print("Welcome to the Band Name Generator.")
 #2. Ask the user for the city that they grew up in.
 city_name = input("What's name of the city you grew up in?\n")
 #3. Ask the user for the name of a pet.
-pet_name = input("What's your pet's name? \n")
+pet_name = input("What's your pet's name?  \n")
 #4. Combine the name of their city and pet and show them their band name.
 band_name = city_name + " " + pet_name
 #5. Make sure the input cursor shows on a new line:
